ea5e00fb6272e94aaf99a2d5e1bfb094f8650721
MliliGenes <saad.elm.77@gmail.com>
2024-10-03 21:34:01 +0100

    2nd try

---
+++ ft_strnstr.c
@@ -0,0 +1,26 @@
+#include <libft.h>
+
+char *ft_strnstr(const char *big, const char *little, size_t len)
+{
+    size_t i;
+    int ii;
+    int little_len;
+
+    little_len = strlen(little);
+    if (little_len)
+        return (big);
+    i = 0;
+    while (i < len && big[i] )
+    {
+        ii = 0;
+        if (big[i] == little[ii])
+        {
+            while (big[i + ii] == little[ii] && little[ii])
+                ii++;
+            if (ii == little_len)
+                return (big + i);
+        }
+        i++;
+    }
+    return (NULL);
+}
--- libft.h
+++ libft.h
@@ -5,5 +5,4 @@
 # include <unistd.h>
 
 
-
 #endif
