83b658f637ba89670fbaeb8511ed5bf0c5a10f5f
MliliGenes <saad.elm.77@gmail.com>
2024-10-03 18:51:42 +0100

    2nd try

--- ft_ strncmp.c
+++ ft_ strncmp.c
@@ -7,7 +7,7 @@ int ft_strncmp(const char *s1, const char *s2, size_t n)
     if (n == 0)
         return 0;
     i = 0;
-    while (i <= n && s1[i] && s1[i] == s2[i])
+    while (i < n && s1[i] && s1[i] == s2[i])
         i++;
     return (s1[i] - s2[i]);
 }
---
+++ memchr.c
@@ -0,0 +1,11 @@
+#include "libft.h"
+
+void *memchr(const void *s, int c, size_t n)
+{
+    unsigned char c;
+    const unsigned char *s;
+    size_t i;
+
+    i=0;
+    
+}
