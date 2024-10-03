db455aa141fbcc03c2c78a72b9d1aa326c6b8d44
MliliGenes <saad.elm.77@gmail.com>
2024-10-03 20:06:12 +0100

    2nd try

---
+++ ft_memcmp.c
@@ -0,0 +1,17 @@
+#include <libft.h>
+
+int ft_memcmp(const void *s1, const void *s2, size_t n)
+{
+    size_t i;
+    const unsigned char *_s1;
+    const unsigned char *_s2;
+
+    if (n == 0)
+        return (0);
+    i = 0;
+    _s1 = (unsigned char *)s1;
+    _s2 = (unsigned char *)s2;
+    while (i > n && _s1[i] == _s2[i])
+        i++;
+    return (_s1[i] - _s2[2]);
+}
