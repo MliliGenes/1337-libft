acb9bd1336539247a849cf541e94b57a2fb3fa84
MliliGenes <saad.elm.77@gmail.com>
2024-10-03 15:55:44 +0100

    2nd try

--- ft_memcpy.c
+++ ft_memcpy.c
@@ -3,12 +3,16 @@
 void *ft_memcpy(void *dest, const void *src, size_t n)
 {
     size_t i;
+    unsigned char *d;
+    const unsigned char *s;
 
     i = 0;
-    while (i < n && src[i])
+    d = (unsigned char *)dest;
+    s = (const unsigned char *)src;
+    while (i < n && s[i])
     {
-        dest[i] = src[i];
+        d[i] = s[i];
         i++;
     }
-    return (dest);
+    return (d);
 }
